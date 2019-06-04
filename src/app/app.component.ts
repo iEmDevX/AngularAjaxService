6585b8e08281a5c42eb1a3b8c7eef7d459f0b1a1
watcharapong sribun <mmxx155@gmail.com>
2019-06-04 17:50:22 +0700

    remove import promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AjaxService } from './services/ajax.service';
-import { promise } from 'protractor';
 
 const URLS = {
   GET_USER: 'https://jsonplaceholder.typicode.com/users'
